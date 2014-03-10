7acbcc86b490e7204f1285a53b29d2a495e86d97
bentbot <liam@hogan.re>
2014-03-10 07:49:21 -0400

    Removed old files

--- bitcoin.js
+++
@@ -1,7 +0,0 @@
-var bitcoin = require('bitcoin');
-var client = new bitcoin.Client('localhost', 8332, '54939106-c9d5-4e20-ab4d-991e5707db9f', '42YellowWarbler');
-
-client.getBalance('*', 6, function(err, balance) {
-  if (err) return console.log(err);
-  console.log('Balance:', balance);
-});
